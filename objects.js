2a6320e43486f1d01743913d15032b75a04fdb3e
Roosevelt Blow <rooseveltblow@gmail.com>
2022-10-19 09:56:59 -0600

    completes lesson 7, adds method chaining

--- objects.js
+++ objects.js
@@ -1,15 +1,15 @@
-const userOne = {
-    email: "rooseveltblow@gmail.com",
-    name: 'Roosevelt',
-    login(){
-        console.log(`${this.email} has logged in.`);
-    },
-    logout(){
-        console.log(`${this.email} has logged out.`);
-    }
-}
+// const userOne = {
+//     email: "rooseveltblow@gmail.com",
+//     name: 'Roosevelt',
+//     login(){
+//         console.log(`${this.email} has logged in.`);
+//     },
+//     logout(){
+//         console.log(`${this.email} has logged out.`);
+//     }
+// }
 
-const prop = 'name';
+// const prop = 'name';
 
 // userOne.email = 'hashbrownz@breakfastcity.com';
 // userOne['email'] = 'hashbrownz@breakfastcity.com';
@@ -17,10 +17,10 @@ const prop = 'name';
 
 // console.log(userOne);
 
-userOne.age = 25;
-userOne.logInfo = function(){
-    console.log(this.name, this.age, this.email);
-};
+// userOne.age = 25;
+// userOne.logInfo = function(){
+//     console.log(this.name, this.age, this.email);
+// };
 
 // userOne.logInfo();
 // userOne.login();
@@ -30,11 +30,28 @@ class User{
     constructor(email, name){
         this.email = email;
         this.name = name;
+        this.score = 0;
     }
     login(){
         console.log(`${this.email} has logged in.`);
+        return this;
     }
     logout(){
         console.log(`${this.email} has logged out.`);
+        return this;
     }
+    updateScore(){
+        this.score++;
+        console.log(`${this.email} score is now ${this.email}`);
+        return this;
+    }
+}
+
+const userOne = new User('rooseveltblow@gmail.com','Roosevelt');
+const userTwo = new User('hashbrownz@thebreakfastclub.com', 'Hashbrownz');
+const userThree = new User('ninja@gaiden.com', 'Ryu Hayabusa');
+
+const users = [userOne, userTwo, userThree];
+for(let user of users){
+    user.login().logout();
 }
